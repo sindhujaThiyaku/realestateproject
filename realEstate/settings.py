5f756c666ff959abe1464f9e8c28926a4c6708ef
Sindhuja <sindhuja.thiyagarajan@nexttechnosolutions.co.in>
2018-06-21 17:32:32 +0530

    requirement changes

--- realEstate/settings.py
+++ realEstate/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'realEstate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+ 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
